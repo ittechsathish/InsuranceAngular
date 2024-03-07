1370be2f25ed3069da192e7352a7c4c4b3ca1fa1
Sathishkumar <ittechsathish@hotmail.com>
2024-03-07 13:19:45 +1100

    Unit test completed For UI-Angular

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -24,6 +25,54 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, InsuranceAngular');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Calculate Monthly Premium'
+    );
+  });
+
+  it('should display error message on empty name', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const component = fixture.componentInstance;
+    component.calculateMonthlyPremiumForm.controls['sumInsured'].setValue(
+      '1000'
+    );
+
+    fixture.detectChanges();
+
+    fixture.debugElement
+      .query(By.css('#sumInsuredInput'))
+      .triggerEventHandler('keyup', null);
+
+    fixture.detectChanges();
+
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('#nameError')?.textContent).toContain(
+      'Please enter your name.'
+    );
+  });
+
+  it('should hide error message on valid name', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const component = fixture.componentInstance;
+    component.calculateMonthlyPremiumForm.controls['sumInsured'].setValue(
+      '1000'
+    );
+
+    component.calculateMonthlyPremiumForm.controls['name'].setValue(
+      'default Name'
+    );
+
+    fixture.detectChanges();
+
+    fixture.debugElement
+      .query(By.css('#sumInsuredInput'))
+      .triggerEventHandler('keyup', null);
+
+    fixture.detectChanges();
+
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('#nameError')?.textContent).not.toContain(
+      'Please enter your name.'
+    );
   });
 });
